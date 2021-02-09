d95af197e09d3ff85e2b86b116f5f748ca18018e
Sebastian Honores <sehonores@sebastiansmbp2.lan>
2021-02-09 11:19:05 +0100

    hello world

---
+++ Shared/devSlopesCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  devSlopesCourseApp.swift
+//  Shared
+//
+//  Created by Sebastian Honores on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct devSlopesCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
